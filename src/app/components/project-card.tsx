0a4584a20c10e23a5d70a99f896280d736d1ee18
André Alves <andre.alves2006@hotmail.com>
2026-02-23 17:55:07 -0300

    chore(ProjectCard): add props and image to ProjectCard

--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -1,16 +1,37 @@
-export default function ProjectCard() {
+import Image from "next/image"
+import Link from "next/link"
+
+type ProjectCardProps = {
+  title: string
+  description: string
+  imageSrc: string
+}
+
+export default function ProjectCard({
+  title, 
+  description,
+  imageSrc
+}: Readonly<ProjectCardProps>) {
   return (
     <div className="flex flex-col gap-4 min-w-2xs max-w-90 p-6 rounded-xl bg-[hsl(0,0%,98%)] dark:bg-radial-[at_0%_0%] dark:from-[#2b2b2b] dark:to-[#0f0f0f] shadow-lg">
-      <div className="w-full aspect-video rounded-xl bg-gray-700">
+      <div className="w-full aspect-video rounded-xl pt-2 px-3 bg-gray-400 shadow-[inset_0_0_4px_1px_rgba(0,0,0,0.2)]">
+        <Image
+          className="h-max rounded-md"
+          src={imageSrc}
+          alt={title}
+          width={400}
+          height={225}
+        />
       </div>
 
-      <h3 className="text-h3">Title</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellat obcaecati libero doloribus veritatis autem eum iusto asperiores odio quae earum quod repudiandae sequi esse dolorum, reiciendis voluptatum est? Laborum?
-      </p>
+      <h3 className="text-h3">{title}</h3>
+      <p>{description}</p>
       
       <div>
-        <button type="button" className="font-bold py-2 px-6 rounded-full bg-brand">Ver projeto</button>
+        <Link
+          className="font-bold py-2 px-6 rounded-full bg-brand"
+          href="#"
+        >Ver projeto</Link>
       </div>
     </div>
   )
--- src/app/components/projects-section.tsx
+++ src/app/components/projects-section.tsx
@@ -5,9 +5,10 @@ export default function ProjectsSection() {
   return (
     <Section id="projects" title="Projects">
       <div className="flex flex-wrap justify-center gap-8 max-w-max mx-auto">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          title="Snake Game"
+          description="Jogo Snake clássico desenvolvido com JavaScript, HTML e CSS."
+          imageSrc="/imgs/snake-project.jpeg" />
       </div>
     </Section>
   )
